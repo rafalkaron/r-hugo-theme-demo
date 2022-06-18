4784fc398d9492228167b96e392ef08fd79fec05
rafalkaron <rafalkaron@gmail.com>
2022-06-18 19:31:40 +0200

    Add a script that updates example site

---
+++ scripts/update-example-site.sh
@@ -0,0 +1,6 @@
+rm -r ../themes/r-hugo-theme/example-site/
+mkdir ../themes/r-hugo-theme/example-site/
+
+cp -R ../content/ ../themes/r-hugo-theme/example-site/content/
+cp -R ../static/ ../themes/r-hugo-theme/example-site/static/
+cp ../config.yaml ../themes/r-hugo-theme/example-site/config.yaml
