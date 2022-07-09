6651c3fe95057cef9568f3e4dcd70c503adae780
rafalkaron <rafalkaron@users.noreply.github.com>
2022-07-09 10:24:27 +0000

    deploy: 2d88b3350f10489bf11e8fef440b79af94e9769f

---
+++ js/toc-indicator.js
@@ -0,0 +1,25 @@
+if (document.documentElement.clientWidth >= 992) {
+  window.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const id = entry.target.querySelector("h2").getAttribute("id");
+          if (entry.intersectionRatio > 0) {
+            document
+              .querySelector(`nav li a[href="#${id}"]`)
+              .parentElement.classList.add("active");
+          } else {
+            document
+              .querySelector(`nav li a[href="#${id}"]`)
+              .parentElement.classList.remove("active");
+          }
+        });
+      },
+      { rootMargin: "-15% 0px 0px 0px" }
+    );
+
+    document.querySelectorAll("section.article-section").forEach((section) => {
+      observer.observe(section);
+    });
+  });
+}
