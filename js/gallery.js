98afdd6c5b20760ce9f17f083993030d32c5ae17
rafalkaron <rafalkaron@users.noreply.github.com>
2022-07-16 17:50:40 +0000

    deploy: d3bdb719c95cf1cc42ee44cefb50d1844502e820

--- js/gallery.js
+++ js/gallery.js
@@ -1,168 +1,135 @@
-const images = document.querySelectorAll(".popup");
+const galleryImages = document.querySelectorAll(".popup");
 
-// Create img modals on click
-images.forEach((img) => {
-  let imgObj = {
-    src: img.getAttribute("full"),
-    title: img.parentElement.querySelector("h6").innerText,
-    desc: img.parentElement.querySelector(".description").innerText,
-    location: img.parentElement.querySelector(".location").getAttribute("href"),
-    alt: img.alt,
-    no: img.parentElement.getAttribute("data-item"),
-    total: img.parentElement.getAttribute("data-total"),
-  };
+// Img details
+let currentGalleryImage;
+let nextGalleryImage;
+let prevGalleryImage;
+const totalGalleryImages = document
+  .querySelector(".popup")
+  .parentElement.getAttribute("data-total");
+const firstGalleryImage = document.querySelector(`#item-1 img`);
+const lastGalleryImage = document.querySelector(
+  `#item-${totalGalleryImages} img`
+);
 
-  img.addEventListener("click", (img) => {
-    displayImgModal(imgObj);
+// UI elements
+const modal = document.querySelector(".modal");
+const body = document.querySelector("body");
+let modalClose = document.querySelector(".modal-close");
+let modalPrevButton = document.querySelector(".modal-prev");
+let modalNextButton = document.querySelector(".modal-next");
+
+class galleryItem {
+  constructor(galleryImage) {
+    this.popupImage = document.querySelector(".to-popup");
+    this.src = galleryImage.getAttribute("data-full");
+    this.title = galleryImage.parentElement.querySelector("h6").innerText;
+    this.desc =
+      galleryImage.parentElement.querySelector(".description").innerText;
+    this.location = galleryImage.parentElement
+      .querySelector(".location")
+      .getAttribute("href");
+    this.alt = galleryImage.alt;
+    this.no = galleryImage.parentElement.getAttribute("data-item");
+  }
+
+  initModal() {
+    body.setAttribute("class", "modal-on");
+    modal.style.display = "flex";
+    this.popupImage.setAttribute("src", this.src);
+    this.popupImage.setAttribute("alt", this.alt);
+
+    let downloadIcon = document.querySelector(".modal-download");
+    downloadIcon.setAttribute("href", this.src);
+
+    let modalTitle = document.querySelector(".modal-title");
+    modalTitle.textContent = this.title;
+
+    let modalDesc = document.querySelector(".modal-description");
+    modalDesc.textContent = this.title;
+
+    let modalLocation = document.querySelector(".modal-location");
+    modalLocation.setAttribute("href", this.location);
+
+    let modalNumber = document.querySelector(".modal-no");
+    modalNumber.textContent = this.no;
+
+    let modalCounter = document.querySelector(".modal-counter");
+    modalCounter.textContent = totalGalleryImages;
+
+    if (this.location !== null) {
+      modalLocation.style.display = "inline";
+    } else {
+      modalLocation.style.display = "none";
+    }
+
+    currentGalleryImage = document.querySelector(`#item-${this.no} img`);
+    this.popupImage.onclick = () => {
+      modal.style.display = "none";
+      body.removeAttribute("class", "modal-on");
+    };
+    nextGalleryImage = document.querySelector(
+      `#item-${parseInt(this.no) + 1} img`
+    );
+    prevGalleryImage = document.querySelector(
+      `#item-${parseInt(this.no) - 1} img`
+    );
+  }
+}
+
+// Iterate over every image in the gallery
+galleryImages.forEach((galleryImage) => {
+  galleryImage.addEventListener("click", () => {
+    galleryItemInstance = new galleryItem(galleryImage);
+    galleryItemInstance.initModal();
   });
 });
 
-let displayImgModal = (imgObj) => {
-  const modal = document.querySelector(".modal");
-  modal.style.display = "flex";
-
-  popupImage = document.querySelector(".to-popup");
-  popupImage.setAttribute("src", imgObj.src);
-
-  popupImage = document.querySelector(".to-popup");
-  popupImage.setAttribute("alt", imgObj.alt);
-
-  downloadIcon = document.querySelector(".modal-download");
-  downloadIcon.setAttribute("href", imgObj.src);
-
-  modalTitle = document.querySelector(".modal-title");
-  modalTitle.textContent = imgObj.title;
-
-  modalDesc = document.querySelector(".modal-description");
-  modalDesc.textContent = imgObj.title;
-
-  modalLocation = document.querySelector(".modal-location");
-  modalLocation.setAttribute("href", imgObj.location);
-
-  modalNumber = document.querySelector(".modal-no");
-  modalNumber.textContent = imgObj.no;
-
-  modalCounter = document.querySelector(".modal-counter");
-  modalCounter.textContent = imgObj.total;
-
-  if (imgObj.location !== null) {
-    modalLocation.style.display = "inline";
-  } else {
-    modalLocation.style.display = "none";
-  }
-
-  modalClose = document.querySelector(".modal-close");
-
-  body = document.querySelector("body");
-  body.setAttribute("class", "modal-on");
-
-  // Close on close button click
-  modalClose.onclick = () => {
-    modal.style.display = "none";
-    body.removeAttribute("class", "modal-on");
-  };
-
-  // Close on image click
-  popupImage.onclick = () => {
-    modal.style.display = "none";
-    body.removeAttribute("class", "modal-on");
-  };
-  // NEXT PREV buttons
-  nextModalNo = parseInt(imgObj.no) + 1;
-  modalNext = document.querySelector(`#item-${nextModalNo} img`);
-  //refactor
-  modalNextButton = document.querySelector(".modal-next");
-  modalNextButton.onclick = () => {
-    if (modalNext !== null) {
-      modalNextObj = {
-        src: modalNext.getAttribute("full"),
-        title: modalNext.parentElement.querySelector("h6").innerText,
-        desc: modalNext.parentElement.querySelector(".description").innerText,
-        location: modalNext.parentElement
-          .querySelector(".location")
-          .getAttribute("href"),
-        alt: modalNext.alt,
-        no: modalNext.parentElement.getAttribute("data-item"),
-        total: modalNext.parentElement.getAttribute("data-total"),
-      };
-      displayImgModal(modalNextObj);
+// Add keybindings
+document.addEventListener("keydown", (e) => {
+  e = e || window.event;
+  if (e.key === "ArrowRight" && nextGalleryImage !== null) {
+    {
+      let nextGalleryItem = new galleryItem(nextGalleryImage);
+      nextGalleryItem.initModal();
     }
-  };
-
-  // Next on left arrow
-  document.addEventListener("keydown", (e) => {
-    e = e || window.event;
-    if (e.key === "ArrowRight") {
-      {
-        if (modalNext !== null) {
-          modalNextObj = {
-            src: modalNext.getAttribute("full"),
-            title: modalNext.parentElement.querySelector("h6").innerText,
-            desc: modalNext.parentElement.querySelector(".description")
-              .innerText,
-            location: modalNext.parentElement
-              .querySelector(".location")
-              .getAttribute("href"),
-            alt: modalNext.alt,
-            no: modalNext.parentElement.getAttribute("data-item"),
-            total: modalNext.parentElement.getAttribute("data-total"),
-          };
-          displayImgModal(modalNextObj);
-        }
-      }
+  } else if (e.key === "ArrowLeft" && prevGalleryImage !== null) {
+    {
+      let prevGalleryItem = new galleryItem(prevGalleryImage);
+      prevGalleryItem.initModal();
     }
-  });
-
-  prevModalNo = parseInt(imgObj.no) - 1;
-  modalprev = document.querySelector(`#item-${prevModalNo} img`);
-  //refactor
-  modalprevButton = document.querySelector(".modal-prev");
-  modalprevButton.onclick = () => {
-    if (modalprev !== null) {
-      modalPrevObj = {
-        src: modalprev.getAttribute("full"),
-        title: modalprev.parentElement.querySelector("h6").innerText,
-        desc: modalprev.parentElement.querySelector(".description").innerText,
-        location: modalprev.parentElement
-          .querySelector(".location")
-          .getAttribute("href"),
-        alt: modalprev.alt,
-        no: modalprev.parentElement.getAttribute("data-item"),
-        total: modalprev.parentElement.getAttribute("data-total"),
-      };
-      displayImgModal(modalPrevObj);
+  } else if (e.key === "ArrowDown") {
+    {
+      let firstGalleryItem = new galleryItem(firstGalleryImage);
+      firstGalleryItem.initModal();
     }
-  };
-
-  // Prev on left arrow
-  document.addEventListener("keydown", (e) => {
-    e = e || window.event;
-    if (e.key === "ArrowLeft") {
-      {
-        if (modalprev !== null) {
-          modalPrevObj = {
-            src: modalprev.getAttribute("full"),
-            title: modalprev.parentElement.querySelector("h6").innerText,
-            desc: modalprev.parentElement.querySelector(".description")
-              .innerText,
-            location: modalprev.parentElement
-              .querySelector(".location")
-              .getAttribute("href"),
-            alt: modalprev.alt,
-            no: modalprev.parentElement.getAttribute("data-item"),
-            total: modalprev.parentElement.getAttribute("data-total"),
-          };
-          displayImgModal(modalPrevObj);
-        }
-      }
+  } else if (e.key === "ArrowUp") {
+    {
+      let lastGalleryItem = new galleryItem(lastGalleryImage);
+      lastGalleryItem.initModal();
     }
-  });
-
-  // Close on ESC
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "Escape") {
+  } else if (e.key === "Escape") {
+    {
       modal.style.display = "none";
       body.removeAttribute("class", "modal-on");
     }
-  });
+  }
+});
+
+// Add mouse events
+modalClose.onclick = () => {
+  modal.style.display = "none";
+  body.removeAttribute("class", "modal-on");
+};
+modalPrevButton.onclick = () => {
+  if (prevGalleryImage !== null) {
+    let prevGalleryItem = new galleryItem(prevGalleryImage);
+    prevGalleryItem.initModal();
+  }
+};
+modalNextButton.onclick = () => {
+  if (nextGalleryImage !== null) {
+    let nextGalleryItem = new galleryItem(nextGalleryImage);
+    nextGalleryItem.initModal();
+  }
 };
